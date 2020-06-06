db2bbee8bf74af38fcce87f8efefd643e96e3c1c
levi1913 <58019791+levi1913@users.noreply.github.com>
2020-06-06 18:23:02 -0500

    levi

---
+++ js/generador.js
@@ -0,0 +1,15 @@
+
+var nombre = document.querySelector(".nombre");
+console.log(screen.width)
+if(screen.width >= 500){
+    nombre.innerHTML == ""
+    for(var i=0;i<770;i++){
+        nombre.innerHTML += (" Dan Levi Torres Morales ");
+     }
+}else{
+    nombre.innerHTML == ""
+    for(var i=0;i<200;i++){
+        nombre.innerHTML += (" Dan Levi Torres Morales ");
+     }
+     console.log("imprimiedo")
+}
